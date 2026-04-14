92aa9a2160b35c9911be8bd7f6135833a51fb316
F2946669 Enzo Romano <enzoromano@bb.com.br>
2026-04-14 16:38:02 -0300

    Pequenos ajustes

--- num_primos2.0.c
+++ num_primos2.0.c
@@ -10,39 +10,42 @@
 int main(){
     //inicialização do arquivo.
     FILE *file;
-    file = fopen("NumerosPrimos.txt", "w");
+    file = fopen("Numerosnaop.txt", "w");
     if (file == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
 
-    // numero maximo até onde calcularemos os primos.
-    // tipo unsigned porque so consideraremos os numeros naturais.
+    //Numero maximo até onde calcularemos os naop.
+    //Tipo unsigned porque so consideraremos os numeros naturais.
     size_t n;
     scanf("%lu", &n);
 
-    //alocacao de um vetor com n booleanos
-    bool *primos;
-    primos = calloc(n, sizeof(bool));
+    //Alocacao de um vetor com n booleanos
+    //Para cada posição, naop diz se o indice é primo
+    bool *naop;
+    naop = calloc(n, sizeof(bool));
 
-    //Quantide de numeros primos encontrados
+    //Quantide de numeros naop encontrados
     size_t qt = 0;
     
-    //variaveis de medição do tempo de execução
+    //Variaveis de medição do tempo de execução
     time_t t_inicio = time(NULL);
     time_t t_fim;
 
+    //Para cada numero, diz que seus multiplos não são primos
     for (size_t i = 2; i < n; i++)
     {
         for (size_t j = 2*i; j < n; j += i)
         {
-            primos[j] = true;
+            naop[j] = true;
         }
     }
 
+    //Se o numero é primo, naop eh falso, portanto ele sera printado no arquivo
     for (size_t i = 2; i < n; i++)
     {
-        if (primos[i] == false)
+        if (naop[i] == false)
         {
             fprintf(file, "%lu - ", i);
             qt++;
@@ -53,13 +56,13 @@ int main(){
         }
     }
 
-    printf("%lu Números Primos\n", qt);
+    printf("%lu Números naop\n", qt);
     t_fim = time(NULL);
     printf(" Tempo de execucao: %lds\n", t_fim - t_inicio);
 
     //Deve-se liberar a memoria alocada dinamicamente e o arquivo 
     //aberto durante a execucao ao fim do programa.
-    free(primos); 
+    free(naop); 
     fclose(file);
     return 0;
 }
